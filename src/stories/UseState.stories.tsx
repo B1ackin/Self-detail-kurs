0af3080d2758d8654e483c154d000fa324b3c53f
B1ackin <xx13.02@gmail.com>
2020-10-27 21:47:51 +0300

    Done 18 lesson

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,27 @@
+import React, {useMemo, useState} from "react";
+
+export default {
+    title: 'UseState demo',
+}
+
+function generateData() {
+    return 1
+}
+
+export const Example1 = () => {
+
+    const initValue = useMemo(generateData,[])
+
+    const [counter, setCounter] = useState(generateData);
+
+    const changer = (state: number) => {
+        return state + 1
+    }
+
+    return <div>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </div>
+}
+
+
