2b2381d7b92199c8c1cee0c0cddf6150a81766a4
B1ackin <xx13.02@gmail.com>
2020-10-28 15:17:16 +0300

    19 lesson in progress

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useMemo, useState} from "react";
+
+export default {
+    title: 'UseEffect demo',
+}
+
+
+export const SimpleExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1);
+
+    console.log("Simple Example");
+
+    useEffect(() => {
+        console.log("useEffect every render");
+        document.title = counter.toString();
+    })
+
+    useEffect(() => {
+        console.log("useEffect only first render (componentDidMount)");
+        document.title = counter.toString();
+    }, [])
+
+    useEffect(() => {
+        console.log("useEffect first render and every counter change");
+        document.title = counter.toString();
+    },[counter])
+
+    return <div>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>Fake +</button>
+        <button onClick={() => setCounter(counter + 1)}>Counter +</button>
+
+    </div>
+}
+
+
